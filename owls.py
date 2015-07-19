d3599cf19133f3b52ff9187648851c9275c1eeee
user <user@lorbanery>
2015-07-19 23:58:48 +0100

    Some owls

---
+++ owls.py
@@ -0,0 +1,46 @@
+"""Helpful code-snippets for creating basic objects in python.
+
+Replace boring code for creating new objects with these more enjoyable
+alternatives!
+
+Boring code:
+>>> visited = set()
+
+Awesome code:
+>>> visited = {0,0} - {0-0}
+
+"""
+# License: https://creativecommons.org/publicdomain/zero/1.0/
+
+
+#
+# Here are a bunch of owls sitting together in a row:
+#
+
+eampty_set = lambda :\
+\
+	{0-0} - {0o0} | {0.0} - {0,0} | {0e0} - {[0][0]}
+
+
+#
+# Goofy people wearing glasses
+#
+
+empty_list = lamba :\
+\
+	 [0,][:0]
+
+empty_dict = lambda :\
+\
+	[{}][0]
+
+
+#
+# Some sort of robot?
+#
+def empty_tuple():
+
+	v,={():()}; v
+
+	return v
+
